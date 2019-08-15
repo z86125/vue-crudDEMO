5dcd27b419335e763205eed884a18542f6cea5be
z86125 <39057726+z86125@users.noreply.github.com>
2019-08-15 17:13:47 +0800

    改一點點

---
+++ index.js
@@ -0,0 +1,20 @@
+var data = [
+    { A: '1', B: 'hello' },
+    { A: '2', B: 'world' },
+    { A: '3', B: 'AI' }
+];
+
+var main = new Vue({
+    el: '#table',
+    data: {
+        table: data
+    },
+    methods: {
+        GetList: function () {
+
+        },
+        DeleteItem: function (item) {
+            
+        }
+    }
+});
